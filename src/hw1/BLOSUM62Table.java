69dc16f3c0fb4e1b500fff5f775bd600af5d93d6
Joyce Zhou <jyzhou15@cs.washington.edu>
2018-10-11 23:48:34 -0700

    init commit

---
+++ src/hw1/BLOSUM62Table.java
@@ -0,0 +1,82 @@
+package hw1;
+
+import java.util.*;
+
+public class BLOSUM62Table {
+	// We can ignore B, Z, X, and * rows
+	private static final char[] INPUT_HEADER = {'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L',
+			'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'/* , 'B', 'Z', 'X', '*' */ };
+
+	private static final String INPUT_TABLE = "A  4 -1 -2 -2  0 -1 -1  0 -2 -1 -1 -1 -1 -2 -1  1  0 -3 -2  0 -2 -1  0 -4 \r\n"
+			+ "R -1  5  0 -2 -3  1  0 -2  0 -3 -2  2 -1 -3 -2 -1 -1 -3 -2 -3 -1  0 -1 -4 \r\n"
+			+ "N -2  0  6  1 -3  0  0  0  1 -3 -3  0 -2 -3 -2  1  0 -4 -2 -3  3  0 -1 -4 \r\n"
+			+ "D -2 -2  1  6 -3  0  2 -1 -1 -3 -4 -1 -3 -3 -1  0 -1 -4 -3 -3  4  1 -1 -4 \r\n"
+			+ "C  0 -3 -3 -3  9 -3 -4 -3 -3 -1 -1 -3 -1 -2 -3 -1 -1 -2 -2 -1 -3 -3 -2 -4 \r\n"
+			+ "Q -1  1  0  0 -3  5  2 -2  0 -3 -2  1  0 -3 -1  0 -1 -2 -1 -2  0  3 -1 -4 \r\n"
+			+ "E -1  0  0  2 -4  2  5 -2  0 -3 -3  1 -2 -3 -1  0 -1 -3 -2 -2  1  4 -1 -4 \r\n"
+			+ "G  0 -2  0 -1 -3 -2 -2  6 -2 -4 -4 -2 -3 -3 -2  0 -2 -2 -3 -3 -1 -2 -1 -4 \r\n"
+			+ "H -2  0  1 -1 -3  0  0 -2  8 -3 -3 -1 -2 -1 -2 -1 -2 -2  2 -3  0  0 -1 -4 \r\n"
+			+ "I -1 -3 -3 -3 -1 -3 -3 -4 -3  4  2 -3  1  0 -3 -2 -1 -3 -1  3 -3 -3 -1 -4 \r\n"
+			+ "L -1 -2 -3 -4 -1 -2 -3 -4 -3  2  4 -2  2  0 -3 -2 -1 -2 -1  1 -4 -3 -1 -4 \r\n"
+			+ "K -1  2  0 -1 -3  1  1 -2 -1 -3 -2  5 -1 -3 -1  0 -1 -3 -2 -2  0  1 -1 -4 \r\n"
+			+ "M -1 -1 -2 -3 -1  0 -2 -3 -2  1  2 -1  5  0 -2 -1 -1 -1 -1  1 -3 -1 -1 -4 \r\n"
+			+ "F -2 -3 -3 -3 -2 -3 -3 -3 -1  0  0 -3  0  6 -4 -2 -2  1  3 -1 -3 -3 -1 -4 \r\n"
+			+ "P -1 -2 -2 -1 -3 -1 -1 -2 -2 -3 -3 -1 -2 -4  7 -1 -1 -4 -3 -2 -2 -1 -2 -4 \r\n"
+			+ "S  1 -1  1  0 -1  0  0  0 -1 -2 -2  0 -1 -2 -1  4  1 -3 -2 -2  0  0  0 -4 \r\n"
+			+ "T  0 -1  0 -1 -1 -1 -1 -2 -2 -1 -1 -1 -1 -2 -1  1  5 -2 -2  0 -1 -1  0 -4 \r\n"
+			+ "W -3 -3 -4 -4 -2 -2 -3 -2 -2 -3 -2 -3 -1  1 -4 -3 -2 11  2 -3 -4 -3 -2 -4 \r\n"
+			+ "Y -2 -2 -2 -3 -2 -1 -2 -3  2 -1 -1 -2 -1  3 -3 -2 -2  2  7 -1 -3 -2 -1 -4 \r\n"
+			+ "V  0 -3 -3 -3 -1 -2 -2 -3 -3  3  1 -2  1 -1 -2 -2  0 -3 -1  4 -3 -2 -1 -4 \r\n"
+			+ "B -2 -1  3  4 -3  0  1 -1  0 -3 -4  0 -3 -3 -2  0 -1 -4 -3 -3  4  1 -1 -4 \r\n"
+			+ "Z -1  0  0  1 -3  3  4 -2  0 -3 -3  1 -1 -3 -1  0 -1 -3 -2 -2  1  4 -1 -4 \r\n"
+			+ "X  0 -1 -1 -1 -2 -1 -1 -1 -1 -1 -1 -1 -1 -1 -2  0  0 -2 -1 -1 -1 -1 -1 -4 \r\n"
+			+ "* -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4  1 ";
+	private final Map<UnorderedPair, Integer> BLOSUM62 = new HashMap<>();
+
+	public BLOSUM62Table() {
+		Scanner scanner = new Scanner(INPUT_TABLE);
+		for (int i = 0; i < INPUT_HEADER.length; i++) {
+			char current = scanner.next().charAt(0);
+			for (int j = 0; j < INPUT_HEADER.length; j++) {
+				BLOSUM62.put(new UnorderedPair(INPUT_HEADER[j], current), scanner.nextInt());
+			}
+			scanner.nextLine();
+		}
+		scanner.close();
+	}
+
+	public Integer lookup(char c1, char c2) {
+		return this.BLOSUM62.get(new UnorderedPair(c1, c2));
+	}
+
+	private class UnorderedPair {
+		private char a;
+		private char b;
+
+		public UnorderedPair(char a, char b) {
+			this.a = a;
+			this.b = b;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (!(o instanceof UnorderedPair)) {
+				return false;
+			}
+
+			UnorderedPair other = (UnorderedPair) o;
+			if (this.a == other.a && this.b == other.b) {
+				return true;
+			} else if (this.a == other.b && this.b == other.a) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		@Override
+		public int hashCode() {
+			return this.a * this.b;
+		}
+	}
+}
---
+++ src/hw1/LocalSequenceAligner.java
@@ -0,0 +1,20 @@
+package hw1;
+
+import java.util.*;
+
+public class LocalSequenceAligner {
+
+	private static final int GAP_PENALTY = -4;
+
+	public static int alignLocalSequence(BLOSUM62Table table, String seq1, String seq2) {
+		// TODO
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		BLOSUM62Table table = new BLOSUM62Table();
+		// TODO
+	}
+
+}
