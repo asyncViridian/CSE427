2734afec40d4f6f49b2f12381f1b05847e34ca99
Joyce Zhou <jyzhou15@cs.washington.edu>
2018-12-10 18:49:38 -0800

    add addPseudo

--- src/hw4/MEME.java
+++ src/hw4/MEME.java
@@ -32,14 +32,18 @@ public class MEME {
         proteins.put(proteinName, proteinContent);
         listNames.add(proteinName);
 
+        // TODO
         // Create original count matrix
         CountMatrix mx = makeCountMatrix(listNames, proteins);
+        FourTuple ps = new FourTuple(3);
+        mx = addPseudo(mx, ps);
     }
 
     /**
      * Generates a count matrix given the list of protein names (and a name->sequence map)
+     *
      * @param listNames names of proteins to include
-     * @param proteins mapping of name to sequence
+     * @param proteins  mapping of name to sequence
      * @return CountMatrix of given proteins
      */
     public static CountMatrix makeCountMatrix(List<String> listNames, Map<String, String> proteins) {
@@ -48,6 +52,17 @@ public class MEME {
             mx.addSequence(seqName, proteins.get(seqName));
         }
         return mx;
+    }
+
+    /**
+     * Returns the sum of mx and pseudo (pseudo added to each line of mx)
+     *
+     * @param mx     CountMatrix to add to
+     * @param pseudo pseudocount vector to add
+     * @return sum of mx and pseudo
+     */
+    public static CountMatrix addPseudo(CountMatrix mx, FourTuple pseudo) {
+        return mx.addVector(pseudo);
     }
 
     /**
@@ -95,6 +110,21 @@ public class MEME {
             FourTuple tup = new FourTuple(seq);
             this.counts.put(seqName, tup);
         }
+
+        /**
+         * Returns the sum of this and vector (vector added to each line of this)
+         *
+         * @param vector pseudocount vector to add
+         * @return sum of this and vector
+         */
+        public CountMatrix addVector(FourTuple vector) {
+            CountMatrix result = new CountMatrix();
+            for (String line : this.counts.keySet()) {
+                result.counts.put(line, this.counts.get(line).add(vector));
+            }
+            return result;
+        }
+
     }
 
 
@@ -145,6 +175,21 @@ public class MEME {
                     throw new RuntimeException("seq " + seq + " has nonstandard DNA base " + letter);
                 }
             }
+        }
+
+        /**
+         * Returns the vector sum of this and other
+         *
+         * @param other other vector to add
+         * @return this+other vector
+         */
+        public FourTuple add(FourTuple other) {
+            FourTuple result = new FourTuple();
+            result.a[0] = this.a[0] + other.a[0];
+            result.a[1] = this.a[1] + other.a[1];
+            result.a[2] = this.a[2] + other.a[2];
+            result.a[3] = this.a[3] + other.a[3];
+            return result;
         }
     }
 }
